a685022c36c640d98f48af08ff5be87fe2b39ed1
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:33 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,52 +3,52 @@ from conans import ConanFile
 
 class BoostLogConan(ConanFile):
     name = "Boost.Log"
-    version = "1.65.1"
+    version = "1.66.0"
 
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
     requires = \
-        "Boost.Align/1.65.1@bincrafters/testing", \
-        "Boost.Array/1.65.1@bincrafters/testing", \
-        "Boost.Asio/1.65.1@bincrafters/testing", \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Atomic/1.65.1@bincrafters/testing", \
-        "Boost.Bind/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Date_Time/1.65.1@bincrafters/testing", \
-        "Boost.Exception/1.65.1@bincrafters/testing", \
-        "Boost.Filesystem/1.65.1@bincrafters/testing", \
-        "Boost.Function_Types/1.65.1@bincrafters/testing", \
-        "Boost.Fusion/1.65.1@bincrafters/testing", \
-        "Boost.Interprocess/1.65.1@bincrafters/testing", \
-        "Boost.Intrusive/1.65.1@bincrafters/testing", \
-        "Boost.Iterator/1.65.1@bincrafters/testing", \
-        "Boost.Lexical_Cast/1.65.1@bincrafters/testing", \
-        "Boost.Locale/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Optional/1.65.1@bincrafters/testing", \
-        "Boost.Parameter/1.65.1@bincrafters/testing", \
-        "Boost.Phoenix/1.65.1@bincrafters/testing", \
-        "Boost.Predef/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Property_Tree/1.65.1@bincrafters/testing", \
-        "Boost.Proto/1.65.1@bincrafters/testing", \
-        "Boost.Range/1.65.1@bincrafters/testing", \
-        "Boost.Regex/1.65.1@bincrafters/testing", \
-        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-        "Boost.Spirit/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.System/1.65.1@bincrafters/testing", \
-        "Boost.Thread/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Index/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing", \
-        "Boost.Winapi/1.65.1@bincrafters/testing", \
-        "Boost.Xpressive/1.65.1@bincrafters/testing"
+        "Boost.Align/1.66.0@bincrafters/testing", \
+        "Boost.Array/1.66.0@bincrafters/testing", \
+        "Boost.Asio/1.66.0@bincrafters/testing", \
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Atomic/1.66.0@bincrafters/testing", \
+        "Boost.Bind/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Date_Time/1.66.0@bincrafters/testing", \
+        "Boost.Exception/1.66.0@bincrafters/testing", \
+        "Boost.Filesystem/1.66.0@bincrafters/testing", \
+        "Boost.Function_Types/1.66.0@bincrafters/testing", \
+        "Boost.Fusion/1.66.0@bincrafters/testing", \
+        "Boost.Interprocess/1.66.0@bincrafters/testing", \
+        "Boost.Intrusive/1.66.0@bincrafters/testing", \
+        "Boost.Iterator/1.66.0@bincrafters/testing", \
+        "Boost.Lexical_Cast/1.66.0@bincrafters/testing", \
+        "Boost.Locale/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Optional/1.66.0@bincrafters/testing", \
+        "Boost.Parameter/1.66.0@bincrafters/testing", \
+        "Boost.Phoenix/1.66.0@bincrafters/testing", \
+        "Boost.Predef/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Property_Tree/1.66.0@bincrafters/testing", \
+        "Boost.Proto/1.66.0@bincrafters/testing", \
+        "Boost.Range/1.66.0@bincrafters/testing", \
+        "Boost.Regex/1.66.0@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.66.0@bincrafters/testing", \
+        "Boost.Spirit/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.System/1.66.0@bincrafters/testing", \
+        "Boost.Thread/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Index/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing", \
+        "Boost.Winapi/1.66.0@bincrafters/testing", \
+        "Boost.Xpressive/1.66.0@bincrafters/testing"
 
     lib_short_names = ["log"]
     is_header_only = False
@@ -66,9 +66,9 @@ class BoostLogConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-log"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
