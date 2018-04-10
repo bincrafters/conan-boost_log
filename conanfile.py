33cd0f6fecbd42e77445db34bddcc6a96e7c23bc
Rene Rivera <grafikrobot@gmail.com>
2018-04-10 09:47:10 -0500

    Initial 1.67.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -6,60 +6,61 @@ from conans import ConanFile, tools
 
 class BoostLogConan(ConanFile):
     name = "boost_log"
-    version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost_log"
+    version = "1.67.0"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     lib_short_names = ["log"]
     is_header_only = False
-    
+
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_align/1.66.0@bincrafters/testing",
-        "boost_array/1.66.0@bincrafters/testing",
-        "boost_asio/1.66.0@bincrafters/testing",
-        "boost_assert/1.66.0@bincrafters/testing",
-        "boost_atomic/1.66.0@bincrafters/testing",
-        "boost_bind/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_core/1.66.0@bincrafters/testing",
-        "boost_date_time/1.66.0@bincrafters/testing",
-        "boost_exception/1.66.0@bincrafters/testing",
-        "boost_filesystem/1.66.0@bincrafters/testing",
-        "boost_function_types/1.66.0@bincrafters/testing",
-        "boost_fusion/1.66.0@bincrafters/testing",
-        "boost_interprocess/1.66.0@bincrafters/testing",
-        "boost_intrusive/1.66.0@bincrafters/testing",
-        "boost_iterator/1.66.0@bincrafters/testing",
-        "boost_lexical_cast/1.66.0@bincrafters/testing",
-        "boost_locale/1.66.0@bincrafters/testing",
-        "boost_move/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_optional/1.66.0@bincrafters/testing",
-        "boost_parameter/1.66.0@bincrafters/testing",
-        "boost_phoenix/1.66.0@bincrafters/testing",
-        "boost_predef/1.66.0@bincrafters/testing",
-        "boost_preprocessor/1.66.0@bincrafters/testing",
-        "boost_property_tree/1.66.0@bincrafters/testing",
-        "boost_proto/1.66.0@bincrafters/testing",
-        "boost_range/1.66.0@bincrafters/testing",
-        "boost_regex/1.66.0@bincrafters/testing",
-        "boost_smart_ptr/1.66.0@bincrafters/testing",
-        "boost_spirit/1.66.0@bincrafters/testing",
-        "boost_static_assert/1.66.0@bincrafters/testing",
-        "boost_system/1.66.0@bincrafters/testing",
-        "boost_thread/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing",
-        "boost_type_index/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing",
-        "boost_utility/1.66.0@bincrafters/testing",
-        "boost_winapi/1.66.0@bincrafters/testing",
-        "boost_xpressive/1.66.0@bincrafters/testing"
+        "boost_array/1.67.0@bincrafters/testing",
+        "boost_assert/1.67.0@bincrafters/testing",
+        "boost_atomic/1.67.0@bincrafters/testing",
+        "boost_bind/1.67.0@bincrafters/testing",
+        "boost_config/1.67.0@bincrafters/testing",
+        "boost_container/1.67.0@bincrafters/testing",
+        "boost_core/1.67.0@bincrafters/testing",
+        "boost_date_time/1.67.0@bincrafters/testing",
+        "boost_exception/1.67.0@bincrafters/testing",
+        "boost_filesystem/1.67.0@bincrafters/testing",
+        "boost_function_types/1.67.0@bincrafters/testing",
+        "boost_fusion/1.67.0@bincrafters/testing",
+        "boost_intrusive/1.67.0@bincrafters/testing",
+        "boost_iterator/1.67.0@bincrafters/testing",
+        "boost_lexical_cast/1.67.0@bincrafters/testing",
+        "boost_locale/1.67.0@bincrafters/testing",
+        "boost_move/1.67.0@bincrafters/testing",
+        "boost_mpl/1.67.0@bincrafters/testing",
+        "boost_optional/1.67.0@bincrafters/testing",
+        "boost_package_tools/1.67.0@bincrafters/testing",
+        "boost_parameter/1.67.0@bincrafters/testing",
+        "boost_phoenix/1.67.0@bincrafters/testing",
+        "boost_predef/1.67.0@bincrafters/testing",
+        "boost_preprocessor/1.67.0@bincrafters/testing",
+        "boost_property_tree/1.67.0@bincrafters/testing",
+        "boost_proto/1.67.0@bincrafters/testing",
+        "boost_range/1.67.0@bincrafters/testing",
+        "boost_regex/1.67.0@bincrafters/testing",
+        "boost_smart_ptr/1.67.0@bincrafters/testing",
+        "boost_spirit/1.67.0@bincrafters/testing",
+        "boost_static_assert/1.67.0@bincrafters/testing",
+        "boost_system/1.67.0@bincrafters/testing",
+        "boost_thread/1.67.0@bincrafters/testing",
+        "boost_throw_exception/1.67.0@bincrafters/testing",
+        "boost_type_index/1.67.0@bincrafters/testing",
+        "boost_type_traits/1.67.0@bincrafters/testing",
+        "boost_utility/1.67.0@bincrafters/testing",
+        "boost_winapi/1.67.0@bincrafters/testing",
+        "boost_xpressive/1.67.0@bincrafters/testing"
     )
 
+    def build_requirements(self):
+        self.build_requires("boost_align/1.67.0@bincrafters/testing")
+        self.build_requires("boost_asio/1.67.0@bincrafters/testing")
+        self.build_requires("boost_interprocess/1.67.0@bincrafters/testing")
 
     def package_info_additional(self):
         if self.options.shared:
@@ -79,12 +80,13 @@ class BoostLogConan(ConanFile):
 
     # BEGIN
 
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    url = "https://github.com/bincrafters/conan-boost_log"
+    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
     license = "BSL-1.0"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.67.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
@@ -112,7 +114,5 @@ class BoostLogConan(ConanFile):
             import boost_package_tools  # pylint: disable=F0401
             boost_package_tools.package_info(self)
         getattr(self, "package_info_additional", lambda:None)()
-
-
 
     # END
