38b7a5490951ca90e13fd688365f56619c928e76
Rene Rivera <grafikrobot@gmail.com>
2018-04-20 15:52:00 -0500

    Templated test_package [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,14 +9,19 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def requirements(self):
+        boost_deps = ['array', 'assert', 'atomic', 'bind', 'config', 'container', 'core', 'date_time', 'exception', 'filesystem', 'function_types', 'fusion', 'intrusive', 'iterator', 'lexical_cast', 'locale', 'log', 'move', 'mpl', 'optional', 'parameter', 'phoenix', 'predef', 'preprocessor', 'property_tree', 'proto', 'range', 'regex', 'smart_ptr', 'spirit', 'static_assert', 'system', 'thread', 'throw_exception', 'type_index', 'type_traits', 'utility', 'winapi', 'xpressive']
+        for lib in boost_deps:
+            self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        
+
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
             if self.settings.os == "Windows":
-                self.run(os.path.join("bin","test_package"))
+                self.run(os.path.join("bin", "test_package"))
             else:
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ['DYLD_LIBRARY_PATH'],os.path.join("bin","test_package")))
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ['DYLD_LIBRARY_PATH'], os.path.join("bin", "test_package")))
